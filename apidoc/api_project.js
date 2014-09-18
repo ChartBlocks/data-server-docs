b7b15e7a6908565024714457b4d4f29fac101049
Arron Woods <aw@chartblocks.com>
2014-09-18 17:40:25 +0100

    Apidoc file for metadata

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,9 +1,9 @@
 define({
-  "name": "chartblocks-data-server-docs",
-  "version": "0.1.0",
-  "description": "REST API docs",
+  "name": "ChartBlocks REST API docs",
+  "version": "1.0.0",
+  "description": "Documentation for using the ChartBlocks REST API to manage charts and data",
   "generator": {
     "version": "0.7.2",
-    "time": "2014-09-18T15:46:28.719Z"
+    "time": "2014-09-18T16:26:37.635Z"
   }
 });
